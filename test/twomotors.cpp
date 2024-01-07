f48881787ec7d672177c1a1d2a65bd5d4882fcd4
tonynpm <72993595+tonynpm@users.noreply.github.com>
2024-01-07 20:53:58 +0000

    Update twomotors.cpp

--- test/twomotors.cpp
+++ test/twomotors.cpp
@@ -5,36 +5,37 @@
 #include <Arduino.h>
 #include <Stepper.h>
 
-short p1 = 12;
-short p2 = 14;
-short p3 = 26;
-short p4 = 27;
 
-short p5 = 25;
-short p6 = 33;
-short p7 = 32;
-short p8 = 35;
+short p1 = 33;
+short p2 = 32;
+short p3 = 13;
+short p4 = 34;
 
+short pp1 = 12;
+short pp2 = 14;
+short pp3 = 27;
+short pp4 = 26;
 
-Stepper PrimaryStepper(2048, p5,p7,p6,p8);
-Stepper SecondaryStepper(2048, p1,p3,p2,p4); //PINS MUST BE IN ORDER OF 1,3,2,4
+Stepper PrimaryStepper(2048, p1,p3,p2,p4); 
+Stepper XrimaryStepper(2048, pp1,pp3,pp2,pp4);
+
+//PINS MUST BE IN ORDER OF 1,3,2,4
 void setup() {
   pinMode(p1, OUTPUT);
   pinMode(p2, OUTPUT);
   pinMode(p3, OUTPUT);
   pinMode(p4, OUTPUT);
-  pinMode(p5, OUTPUT);
-  pinMode(p6, OUTPUT);
-  pinMode(p7, OUTPUT);
-  pinMode(p8, OUTPUT);
-  SecondaryStepper.setSpeed(20);
+  pinMode(pp1, OUTPUT);
+  pinMode(pp2, OUTPUT);
+  pinMode(pp3, OUTPUT);
+  pinMode(pp4, OUTPUT);
   PrimaryStepper.setSpeed(20);
-
+  XrimaryStepper.setSpeed(20);
 }
 
 void loop(){
-  SecondaryStepper.step(2048);
-  PrrimaryStepper.step(-2048);
-  delay(20);
+  PrimaryStepper.step(2048);
+  XrimaryStepper.step(2048);
+  delay(50);
 }
   
