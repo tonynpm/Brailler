d8d8997fb255380cc702f69dfdb9554071374c8a
tonynpm <72993595+tonynpm@users.noreply.github.com>
2024-01-07 21:40:19 +0000

    Update mapFunction.h by changing assignment operators

--- working/mapFunction.h
+++ working/mapFunction.h
@@ -2,25 +2,25 @@
 
 int mapFunction(int total){
     int x = 0;
-    if(total = 1){
+    if(total == 1){
         x = 1;
     }
-    else if(total = 10){
+    else if(total == 10){
         x = 2;
     }
-    else if(total = 11){
+    else if(total == 11){
         x = 3;
     }
-    else if(total = 100){
+    else if(total == 100){
         x = 4;
     }
-    else if(total = 101){
+    else if(total == 101){
         x = 5;
     }
-    else if(total = 110){
+    else if(total == 110){
         x = 6;
     }
-    else if(total = 111){
+    else if(total == 111){
         x = 7;
     }
     return x;
