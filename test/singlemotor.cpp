a8473032365178ebbe6b6a680338881f7fd567b8
tonynpm <72993595+tonynpm@users.noreply.github.com>
2024-01-07 16:18:22 +0000

    Update singlemotor.cpp

--- test/singlemotor.cpp
+++ test/singlemotor.cpp
@@ -1,1 +1,27 @@
+//2048 steps per revolution
+//2ms delay per step
+//500 steps per second
+
+#include <Arduino.h>
+#include <Stepper.h>
+
+short p1 = 12;
+short p2 = 14;
+short p3 = 26;
+short p4 = 27;
+
+Stepper SecondaryStepper(2048, p1,p2,p3,p4);
+void setup() {
+  pinMode(p1, OUTPUT);
+  pinMode(p2, OUTPUT);
+  pinMode(p3, OUTPUT);
+  pinMode(p4, OUTPUT);
+  SecondaryStepper.setSpeed(20);
+}
+
+void loop(){
+  SecondaryStepper.step(2048);
+  delay(1000);
+}
+  
 
